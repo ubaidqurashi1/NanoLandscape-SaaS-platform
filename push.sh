d452b4c3ad274d7635f6e4040069fab322954201
Syed Ubaid Qurashi <qurashiubaid@gmail.com>
2026-04-21 15:27:58 +0530

    Add files via upload

---
+++ push.sh
@@ -0,0 +1,11 @@
+# Add all files to git
+git add .
+
+# Check status
+git status
+
+# Commit all changes
+git commit -m "Initial commit: Complete NanoLandscape SaaS Platform implementation"
+
+# Push to GitHub
+git push origin main
